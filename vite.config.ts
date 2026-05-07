3bdb00b4b87e82c2706a0aba3130d6c7bd9428b7
Websprint <websprintt@gmail.com>
2026-05-07 19:00:00 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
+  base: '/Marquez-Instalaciones/',
   const env = loadEnv(mode, '.', '');
   return {
     base: './',
